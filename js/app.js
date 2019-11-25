6d78c1f5a37e5e00225ff549c568fcd80026b915
Tywon Berry <dtywon601@gmail.com>
2019-11-25 10:36:05 -0600

    updated Math Calculator

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+document.addEventListener("keyup", function () {
+    
+    let keycode = event.keyCode;
+    if (keycode >= 96 && keycode <= 105 || keycode >= 48 && keycode <= 57) {
+        self.insert(event.key);
+    }
+    if ([111, 106, 109, 107, 13].includes(keycode)) {
+        if(keycode == 13) {
+            equal();
+        } else {
+        self.insert(event.key, 'keyOperator');
+        }
+    }
+    if (keycode == 8) {
+        back();
+    }
+    console.log(event);
+});
+
 function insert(num) {
     document.form.numberDisplay.value = document.form.numberDisplay.value+num;
 }
@@ -17,3 +36,5 @@ function equal() {
         document.form.numberDisplay.value = eval(value)
     }
 }
+
+// event listeners and coding out the logic of the calculator
